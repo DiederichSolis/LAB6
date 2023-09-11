937096773af9b1b96901e53847ab0302519a5395
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-09-11 14:08:11 -0600

    cambio y correcciones

--- app/src/main/java/com/diederich/lab_6/ui/theme/Color.kt
+++ app/src/main/java/com/diederich/lab_6/ui/theme/Color.kt
@@ -6,6 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
-
+val pink = Color(255, 229, 236)
+val purple1 = Color(213, 198, 224)
 val barra = Color(222, 217, 226)
 val cc = Color(225, 197, 209)
